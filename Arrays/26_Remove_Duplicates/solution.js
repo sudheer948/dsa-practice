5418a16ece61373c995377cf3cf91efb5adeffd1
Muhammed Sudheer A.S <muhammedsudheer948@gmail.com>
2026-02-26 21:27:00 +0530

    Remove Duplicates: Final Code, Complexity & Interview Notes

---
+++ Arrays/26_Remove_Duplicates/solution.js
@@ -0,0 +1,16 @@
+// Final Code (Javascript)
+
+```javascript
+var removeDuplicates = function(nums) {
+    let x = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > nums[x]) {
+            x++;
+            nums[x] = nums[i];
+        }
+    }
+
+    return x + 1;
+};
+```
