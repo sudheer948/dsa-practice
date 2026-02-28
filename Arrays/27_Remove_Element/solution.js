0eaf9658c78df6a33057e1d4a50b07c6f800edb2
Muhammed Sudheer A.S <muhammedsudheer948@gmail.com>
2026-02-28 15:10:32 +0530

    Remove Element: Final Code, Complexity & Pattern

---
+++ Arrays/27_Remove_Element/solution.js
@@ -0,0 +1,12 @@
+var removeElement = function(nums, val) {
+    let x = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[x] = nums[i];
+            x++;
+        }
+    }
+
+    return x;
+};
